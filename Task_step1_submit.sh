740d41cd646af8ebedf18c143afdb4499174cf79
nmuncy <pingamormn@gmail.com>
2019-05-14 09:04:03 -0600

    don't repeat

--- Task_step1_submit.sh
+++ Task_step1_submit.sh
@@ -12,16 +12,19 @@ slurmDir=${workDir}/derivatives/Slurm_out
 time=`date '+%Y_%m_%d-%H_%M_%S'`
 outDir=${slurmDir}/TS1_${time}
 
-
 mkdir -p $outDir
 
+
 cd ${workDir}/derivatives
+
 for i in sub*; do
+	if [ ! -f ${i}/run-1_Test_scale+tlrc.HEAD ]; then
 
-    sbatch \
-    -o ${outDir}/output_TS1_${i}.txt \
-    -e ${outDir}/error_TS1_${i}.txt \
-    ${scriptDir}/Task_step1_sbatch_preproc.sh $i
+	    sbatch \
+	    -o ${outDir}/output_TS1_${i}.txt \
+	    -e ${outDir}/error_TS1_${i}.txt \
+	    ${scriptDir}/Task_step1_sbatch_preproc.sh $i
 
-    sleep 1
+	    sleep 1
+	fi
 done
