c055c3981fe1e419c1f6c5870b7a35ee7760b47b
nmuncy <pingamormn@gmail.com>
2019-06-05 11:34:31 -0600

    changed trial duration, updated notes

--- Beh_step2_timing.R
+++ Beh_step2_timing.R
@@ -1,7 +1,11 @@
 
 
 
-##### Set up
+### Set up
+# Orienting variables and paths for the location of the raw
+# data and output dir.
+# References output made by Beh_step0_mkList.sh
+
 parDir <- '/Volumes/Yorick/Temporal/Experiment3/'
 workDir <- paste0(parDir,"Behavioral_Data/")
 outDir <- paste0(parDir,"Analyses/behAnalysis/timing_files/")
@@ -9,38 +13,36 @@ outDir <- paste0(parDir,"Analyses/behAnalysis/timing_files/")
 subjList <- read.delim(paste0(workDir,"Subj_List.txt"),header = F)
 subjList <- t(subjList)
 
-# I didn't have PsychoPy separate output according to training session or blocks
+# Hardcoded - forgot to have psychopy write the length of 
+# training session and block numbers
 trainLength <- 12
 numBlock <- 3
 
 
 
-# j<- subjList[2]
+
+### Get data for e/subject
 for(j in subjList){
   
   # load data
   subjDir <- paste0(workDir,j,"/")
   df.subj <- read.csv(paste0(subjDir,j,"_timing_log.csv"))
   df.subj <- as.data.frame(df.subj)
-  
-  
-  ### Determine positions, account for missing/multiple responses
-  # Find start of each trial, pull info from known positions following trial start.
-  # Multiple/no responses will have a different count following trial start, so
-  # account for that to not get off count.
-  # Clean up data, remove training session, determine length of e/block.
-  
-  # set variables
-  hold_list <- c("stim_onset","stat_onset","stim_dur","stim_typ","stim_lag","stim_rep","resp_onset","response")
-  for(a in hold_list){
-    assign(paste0("hold_",a),NA)
-  }
-  
+
   # determine start/end positions
   ind_trial_start <- grep("Trial Start",df.subj[,1])
   ind_trial_end <- grep("Trial End",df.subj[,1])
   
-  # fill variables from known positions
+  # get desired variables ready
+  hold_list <- c("stim_onset","stat_onset","stim_dur","stim_typ","stim_lag","stim_rep","resp_onset","response","resp_time")
+  for(a in hold_list){
+    assign(paste0("hold_",a),NA)
+  }
+  
+  
+  ## Fill variables from known positions.
+  # This is based on the order of the psychopy output,
+  # referencing the start position for each trial
   for(i in 1:length(ind_trial_start)){
     
     hold_stim_onset <- c(hold_stim_onset,round(as.numeric(as.character(df.subj[(ind_trial_start[i]+1),2])),1))
@@ -50,8 +52,11 @@ for(j in subjList){
     hold_stim_lag <- c(hold_stim_lag,as.character(df.subj[(ind_trial_start[i]+5),2]))
     hold_stim_rep <- c(hold_stim_rep,as.character(df.subj[(ind_trial_start[i]+6),2]))
     hold_resp_onset <- c(hold_resp_onset,round(as.numeric(as.character(df.subj[(ind_trial_start[i]+7),2])),1))
+    hold_resp_time <- c(hold_resp_time,round(as.numeric(as.character(df.subj[(ind_trial_start[i]+8),2])),1))
 
-    # determine single/multiple/no response, fill accordingly (account for last trial)
+    
+    ## Determine single/multiple/no response, fill accordingly, & account for last trial
+    # Only 11 lines should be written for each trial if participant responded once
     hold1 <- ind_trial_start[i]+9; hold2 <- ind_trial_start[i]+11
     if(grepl("Response",df.subj[hold1,1])==T && (grepl("Trial Start",df.subj[hold2,1])==T || hold2 >= dim(df.subj)[1])){
       hold_response <- c(hold_response,as.character(df.subj[hold1,2]))
@@ -60,12 +65,12 @@ for(j in subjList){
     }
   }
   
-  # remove NA
+  # remove first NAs
   for(a in hold_list){
     assign(paste0("hold_",a),(get(paste0("hold_",a))[-1]))
   }
   
-  # remove spaces
+  # remove spaces from some variables
   for(b in 1:length(hold_stim_dur)){
     hold_response[b] <- gsub(" ","",hold_response[b],fixed=T)
     hold_stim_dur[b] <- gsub(" ","",hold_stim_dur[b],fixed=T)
@@ -73,14 +78,11 @@ for(j in subjList){
     hold_stim_rep[b] <- gsub(" ","",hold_stim_rep[b],fixed=T)
     hold_stim_typ[b] <- gsub(" ","",hold_stim_typ[b],fixed=T)
   }
+
   
-  # check
-  for(a in hold_list){
-    h.test <- length(get(paste0("hold_",a)))
-    stopifnot(h.test == length(ind_trial_start))
-  }
-  
-  # remove training, if training happened - this is hard coded
+  ## Remove training responses if training happened 
+  # Experiment length with (294) and without (282) training is hardcoded
+  # Break of wrong number of trials
   stop = FALSE
   if(length(hold_response)==294){
     
@@ -96,7 +98,7 @@ for(j in subjList){
   }
   if(stop){break}
 
-  # get start/end times
+  # get start/end times - hold_trial_end was used in duration calc but now hold_resp_time is
   hold_trial_start <- round(as.numeric(as.character(df.subj[ind_trial_start,2])),1)
   hold_trial_end <- round(as.numeric(as.character(df.subj[ind_trial_end,2])),1)
   
@@ -104,11 +106,13 @@ for(j in subjList){
   blockLength <- length(ind_trial_start)/numBlock
   
 
+  
+  
   ### Behaviors 
-  # This is hardcoded to avoid a conditional nightmare.
-  # Have the variable name be called ind_beh_Foo.
+  # This is hardcoded to avoid a conditional nightmare - hooray for "which"!
+  # Name variables ind_beh_Foo for later use
   # T1=1.5s, T2=1s
-  # Combine behaviors since some bins are very small
+  # Combine behaviors (ind_beh_Foo_ALL) since some bins are very small
   
   # Targets
   ind_beh_Miss_All <- which(hold_stim_typ=="Targ" & hold_stim_rep=="Yes" & (hold_stim_dur=="T1" | hold_stim_dur=="T2") & (hold_response==1 | hold_response==3))
@@ -125,45 +129,23 @@ for(j in subjList){
   ind_beh_FA_All <- sort(c(ind_beh_FA_Long_L1,ind_beh_FA_Same_L1,ind_beh_FA_Same_L2,ind_beh_FA_Short_L2))
   ind_beh_CR_All <- sort(c(ind_beh_CR_Long_L2,ind_beh_CR_Short_L1))
   
-  
-  # ### Determine encoding time that precedes behavior
-  # 
-  # # read in block stim sets
-  # for (i in 1:numBlock){
-  #   
-  #   hold_df_stim <- read.delim(paste0(subjDir,"/",j,"_B",i,"_stimuli.txt"), header=T, sep="\t")
-  #   df_1st_present <- subset(hold_df_stim, hold_df_stim$Repeat == "No" & (hold_df_stim$StimType == "Targ" | hold_df_stim$StimType == "Lure"))
-  #   
-  #   # subset ind_beh by block
-  #   ind_block <- ind_beh_Hit_All[ind_beh_Hit_All<94]
-  
-  #   # pull images of behavior for block
-  #   hold_images <- hold_df_stim[ind_block,3]
-  
-  #   #match images to 1st presentation
-  #   hold_match <- match(hold_image, df_1st_present[,3])
-  # 
-  # }
-  # 
+
   
   
-  
-  ### Write Timing Files
-  # Separate for each block by determining if index is within range,
-  # Use indices to keep things straight
-  # Make sure the beginning of each block has t=0.
-  # Determine start:duration of each behavior coded above.
-  
+  ### Determine start:dur for each behavior
+  # Separate for each block by determining if index is within range
+  # Use indices to keep trials straight
+  # Make sure the beginning of each block has t=0
+
+  # get behavior variables
   beh_list <- gsub("ind_beh_","",ls(pattern="ind_beh"))
   
-  # a <- beh_list[1]
   for(a in beh_list){
     
-    # behavior indices
+    # behavior positions (index)
     ind_a <- get(paste0("ind_beh_",a))
 
     # separate by block
-    # block <- 1
     for(block in 1:numBlock){
       
       # range, start of block
@@ -171,7 +153,7 @@ for(j in subjList){
       UB <- block*blockLength
       time_zero <- round(as.numeric(as.character(df.subj[ind_trial_start[LB],2])),1)
       
-      # import block stim set, determine 1st presentations
+      # pull raw block stim set, determine 1st presentations. subset command references df colnames
       hold_df_stim <- read.delim(paste0(subjDir,"/",j,"_B",block,"_stimuli.txt"), header=T, sep="\t")
       df_1st_present <- subset(hold_df_stim, hold_df_stim$Repeat == "No" & (hold_df_stim$StimType == "Targ" | hold_df_stim$StimType == "Lure"))
       row.names(df_1st_present) <- 1:dim(df_1st_present)[1]
@@ -181,33 +163,32 @@ for(j in subjList){
       hold_block_precede <- paste0("TF_line",block,"_Rp",a)
       assign(hold_block,NA); assign(hold_block_precede,NA)
       
+      
       ## Split behavior into blocks
-      # The first-presentation info is read in from stimulus text file.
-      # It is already broken into blocks, but the behaviors are not, so
+      # The first-presentation info was read in from stimulus text file.
+      # It is already broken into blocks, but the behaviors are not so
       # these have to be matched up.
       
-      # b <- ind_a[1]
       for(b in ind_a){
         if(b %in% seq(LB,UB)==T){
         
-          # Determine the the position within block from "b" which is in total
-          # experiment time. Extract the image string with which the behavior occured 
-          # from stimulus file, then find the index and trial number of the first 
-          # presentation of image.
+          # Determine the position within block from "b", which is in total
+          # experiment time. Extract the image name with which behavior occured, 
+          # then find the index and trial number of the first presentation of image.
           block_ind <- b-((block-1)*blockLength)
           hold_image <- as.character(hold_df_stim[block_ind,3])
           ind_match <- match(hold_image, df_1st_present[,3])
           trial_num <- df_1st_present[ind_match,1]
         
-          # determine start, duration of behavior
+          # determine start, duration of behavior (dur = start to decision, not entire available response time)
           hold_start <- round((hold_resp_onset[b]-time_zero),1)
-          hold_dur <- round(((hold_trial_end[b]-time_zero)-hold_start),1)
+          hold_dur <- round(((hold_resp_time[b]-time_zero)-hold_start),1)
           TF_input <- paste0(hold_start,":",hold_dur)
           
           # determine start, duration of encoding (Response preceding behavior)
           total_trial_num <- trial_num+LB-1
           hold_start_enc <- round((hold_resp_onset[total_trial_num]-time_zero),1)
-          hold_dur_enc <- round(((hold_trial_end[total_trial_num]-time_zero)-hold_start_enc),1)
+          hold_dur_enc <- round(((hold_resp_time[total_trial_num]-time_zero)-hold_start_enc),1)
           TF_input_enc <- paste0(hold_start_enc,":",hold_dur_enc)
         
           # write block timing
@@ -216,7 +197,7 @@ for(j in subjList){
         }
       }
       
-      # remove NA
+      # remove NAs
       assign(hold_block,get(hold_block)[-1])
       assign(hold_block_precede,get(hold_block_precede)[-1])
     }
@@ -238,12 +219,16 @@ for(j in subjList){
   }
   
   
+  
+  
   ### Write Timing Files
-  # Only write out "All" TFs
+  # Only write out "All" TFs due to small bins
+  # Write one line per fMRI run/block
   
   for (out in beh_list) {
     if(grepl("All",out)==T){
       
+      # behavior TFs
       hold1 <- get(paste0("TF_line1_",out))
       hold2 <- get(paste0("TF_line2_",out))
       hold3 <- get(paste0("TF_line3_",out))
@@ -253,6 +238,7 @@ for(j in subjList){
       cat(hold2, "\n", file=outFile1, append=T, sep='\t')
       cat(hold3, "\n", file=outFile1, append=T, sep='\t')
       
+      # encoding preceding behavior TFs
       hold4 <- get(paste0("TF_line1_Rp",out))
       hold5 <- get(paste0("TF_line2_Rp",out))
       hold6 <- get(paste0("TF_line3_Rp",out))
