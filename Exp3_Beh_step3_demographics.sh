62a58b967236f44b84da9613007fd1bd31084d43
nmuncy <pingamormn@gmail.com>
2019-09-30 19:44:17 -0600

    demographics scripts

---
+++ Exp3_Beh_step3_demographics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+parDir=/Volumes/Yorick/Temporal
+expDir=${parDir}/Experiment3/Behavioral_Data
+outDir=${parDir}/Analyses/Exp3/behAnalysis
+
+
+print=${outDir}/Master_demographics.txt
+echo -e "Subj\tAge\tSex" > $print
+
+
+cd $expDir
+for i in s*; do
+
+	age=`cat ${i}/${i}_timing_log.csv | grep "Subject Age" | sed 's/.*\ //'`
+	tmp=`cat ${i}/${i}_timing_log.csv | grep "Subject Sex" | sed 's/.*\ //'`
+	sex=`echo ${tmp:0:1} | tr '[:lower:]' '[:upper:]'`
+
+	if [ ! -z $age ]; then
+		echo -e "${i}\t${age:0:2}\t${sex} " >> $print
+	fi
+done
